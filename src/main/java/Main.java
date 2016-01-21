8beeba37fed87839fefcccb98b9a9faddf1a01f4
HubertLegec <hubert.legec@gmail.com>
2016-01-21 15:02:47 +0100

    gui adjustment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 import backend.TextAnalyzer;
 import backend.TextAnalyzerImpl;
-
 import javafx.application.Application;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
@@ -11,24 +10,34 @@ import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
-import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import org.encog.ml.data.MLData;
 import org.encog.neural.networks.training.propagation.resilient.ResilientPropagation;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main extends Application {
+    TextAnalyzer textLearner = new TextAnalyzerImpl(0.1d, 50, ResilientPropagation.class);
     private Stage primaryStage;
     private BorderPane rootLayout;
     private BooleanProperty readyToWork = new SimpleBooleanProperty(false);
 
     @FXML
-    private TextField directoryTF;
+    private TextField directory1TF;
     @FXML
-    private Button directoryButton;
+    private Button directory1Button;
     @FXML
-    private Button processButton;
+    private Button process1Button;
+    @FXML
+    private TextField directory2TF;
+    @FXML
+    private Button directory2Button;
+    @FXML
+    private Button process2Button;
     @FXML
     private TextArea messageTextArea;
     @FXML
@@ -43,10 +52,6 @@ public class Main extends Application {
     private MenuItem menuClose;
 
     public static void main(String[] args) {
-       TextAnalyzer textLearner = new TextAnalyzerImpl(0.1d, 50, ResilientPropagation.class);
-        textLearner.loadCategoriesFromFile("test/categories.txt");
-        textLearner.trainStemmedFile("test/nauka.txt");
-
         launch(args);
     }
 
@@ -80,12 +85,11 @@ public class Main extends Application {
         resultTextField.setEditable(false);
         messageTextArea.disableProperty().bind(readyToWork.not());
         categorizeButton.disableProperty().bind(readyToWork.not().or(messageTextArea.textProperty().isEmpty()));
-        processButton.disableProperty().bind(directoryTF.textProperty().isEmpty());
+        process2Button.disableProperty().bind(directory2TF.textProperty().isEmpty());
         menuReset.disableProperty().bind(readyToWork.not());
 
         menuReset.setOnAction( event -> {
-            //TODO
-            //zresetowanie tego czego nauczyla sie siec neuronowa
+
             readyToWork.setValue(false);
         });
 
@@ -93,26 +97,39 @@ public class Main extends Application {
             primaryStage.close();
         });
 
-        directoryButton.setOnAction(event -> {
-            System.out.println("event");
-            DirectoryChooser chooser = new DirectoryChooser();
-            File chosenDirectory = chooser.showDialog(rootLayout.getScene().getWindow());
-            if (chosenDirectory != null) {
-                directoryTF.setText(chosenDirectory.getAbsolutePath());
+        directory1Button.setOnAction(event1 ->{
+            FileChooser chooser = new FileChooser();
+            File chosenFile =  chooser.showOpenDialog(rootLayout.getScene().getWindow());
+            if(chosenFile != null){
+                directory1TF.setText(chosenFile.getAbsolutePath());
             }
         });
 
-        processButton.setOnAction(event -> {
-            infoLabel.setText("");
-            File dir = new File(directoryTF.getText());
-            if (!dir.exists()) {
-                Alert alert = new Alert(Alert.AlertType.ERROR, "Type valid directory path!");
+        process1Button.setOnAction(event1 -> {
+            File file = new File(directory1TF.getText());
+            if(!file.exists()){
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Type valid file path!");
+                alert.show();
+            } else{
+                textLearner.loadCategoriesFromFile(directory1TF.getText());
+            }
+        });
+
+        directory2Button.setOnAction(event -> {
+            FileChooser chooser = new FileChooser();
+            File chosenFile = chooser.showOpenDialog(rootLayout.getScene().getWindow());
+            if (chosenFile != null) {
+                directory2TF.setText(chosenFile.getAbsolutePath());
+            }
+        });
+
+        process2Button.setOnAction(event -> {
+            File file = new File(directory2TF.getText());
+            if (!file.exists()) {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Type valid file path!");
                 alert.show();
             } else {
-                //TODO
-                //tutaj uruchomić uczenie
-                //zmienna dir zawiera pliki xml do przetworzenia
-                boolean result = true; // jesli operacja sie nie powiedzie ustawic na fail
+                boolean result = textLearner.trainStemmedFile(directory2TF.getText());
                 if(result) {
                     infoLabel.setTextFill(Paint.valueOf(Color.GREEN.toString()));
                     infoLabel.setText("PROCESSING SUCCEED");
@@ -127,10 +144,22 @@ public class Main extends Application {
         categorizeButton.setOnAction(event -> {
             String message = messageTextArea.getText();
 
-            //TODO
-            //tutaj wywołać funkcję zwracającą kategorię
+            MLData data = textLearner.analyzeText(message);
+            int position = 0;
+            for(int i = 0; i < data.getData().length; i++){
+                if(data.getData()[i] > data.getData()[position]){
+                    position = i;
+                }
+            }
 
-            String result = "AAA"; //tu przypisać rezultat
+            final Integer pos = new Integer(position);
+
+
+            String result = textLearner.getCategories().entrySet()
+                    .stream()
+                    .filter(entry -> entry.getValue() == pos)
+                    .map(Map.Entry::getKey)
+                    .collect(Collectors.toList()).get(0);
             resultTextField.setText(result);
         });
     }
